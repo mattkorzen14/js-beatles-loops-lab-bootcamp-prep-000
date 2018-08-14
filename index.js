54e0155f92c48c36d645bab1cf402878a31af6b6
Matthew Korzen <mattkorzen14@gmail.com>
2018-08-14 20:47:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function johnLennonFacts(facts) {
   }
   return facts;
 }
+function iLoveTheBeatles(number) {
+  let array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while(number < 15);
+  return array;
+}
